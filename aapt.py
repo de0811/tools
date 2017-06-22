003965026e0f43512159a5ef172f91f0c179fabc
num <de0811@naver.com>
2017-06-23 00:30:46 +0900

    config/__init__.py Error , aapt cmd Process Modify

--- aapt.py
+++ aapt.py
@@ -12,8 +12,15 @@ init()
 print(Fore.YELLOW + Back.BLUE + Style.BRIGHT + "aapt.py runing" + Fore.RESET + Back.RESET + Style.NORMAL)
 
 def RunProcess(cmd):
+    print(Fore.BLUE + Back.LIGHTCYAN_EX + Style.BRIGHT + cmd + Fore.RESET + Back.RESET + Style.NORMAL)
     cmd_args = cmd.split()
     pipe = Popen(cmd_args, stdout=PIPE)
+    while pipe.poll() is None:
+        pass
+    if pipe.poll() == 1:
+        print(Fore.RED + Style.BRIGHT + "Error" + Fore.RESET + Style.NORMAL)
+        sys.exit()
+
     outList = pipe.stdout.readlines()
     return outList
 
@@ -23,6 +30,6 @@ if __name__ == "__main__":
     apkInfos = RunProcess(config.aapt + ' ' + sys.argv[1])
     for info in apkInfos:
         if "package:" in info \
-		or "sdkVersion:" in info \
-		or "targetSdkVersion:" in info :
-            print(Fore.YELLOW + info + Fore.RESET)
+                or "sdkVersion:" in info \
+                or "targetSdkVersion:" in info :
+            print(Fore.YELLOW + Style.BRIGHT + info + Fore.RESET + Style.NORMAL)
