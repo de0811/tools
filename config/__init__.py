c172820cc9a05f72aa6ca58ad4bc715cd0fd6781
num <de0811@naver.com>
2017-11-01 18:33:15 +0900

    config Setting

--- config/__init__.py
+++ config/__init__.py
@@ -1,12 +1,21 @@
-apktool = '''java -jar C:\\utill\\apktool.jar'''
+import os
+import platform
+
+isWindows = 'Windows' in platform.platform()
+currentPath = os.path.dirname(os.path.realpath(__file__))
+utilPath = currentPath + "/../util/"
+
+apktool = '''java -jar ''' + utilPath + '''apktool.jar'''
 signer = '''jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore'''
-key = '''C:\\utill\\keystore\\testsig.jks'''
+key = utilPath + '''keystore/testsig.jks'''
 keyPass = '''-storepass 123456'''
 alias = '''testsig'''
 
-jadx = '''/home/numa/Android/utill/jadx/bin/jadx-gui'''
+jadx = utilPath + '''jadx/bin/jadx-gui'''
 bytecode = '''C:\\utill\\BytecodeViewer.2.9.8\\BytecodeViewer.exe'''
 jad = '''C:\\utill\\jd-gui\\jad'''
 
-screencapPath = '''C:\\temp\\ScreenCap'''
-aapt = '''C:\\Android\\sdk\\build-tools\\25.0.3\\aapt.exe dump badging'''
+screencapPath = '''~/temp/ScreenCap'''
+aapt = utilPath + "aapt.exe dump badging"
+if isWindows is False :
+    aapt = utilPath + '''aapt dump badging'''
