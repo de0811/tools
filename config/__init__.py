a6fa155cf10a2aa4403ff7cbcf8ff0f6248bd414
num <de0811@naver.com>
2017-11-01 18:41:54 +0900

    config windows add

--- config/__init__.py
+++ config/__init__.py
@@ -15,7 +15,9 @@ jadx = utilPath + '''jadx/bin/jadx-gui'''
 bytecode = '''C:\\utill\\BytecodeViewer.2.9.8\\BytecodeViewer.exe'''
 jad = '''C:\\utill\\jd-gui\\jad'''
 
-screencapPath = '''~/temp/ScreenCap'''
+screencapPath = '''c:\\temp\\ScreenCap'''
+if isWindows is False :
+    screencapPath = '''~/temp/ScreenCap'''
 aapt = utilPath + "aapt.exe dump badging"
 if isWindows is False :
     aapt = utilPath + '''aapt dump badging'''
