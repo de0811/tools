f96a3a5b979f43572c2fbfd98c5a6aae83b331e2
num <de0811@naver.com>
2018-05-16 17:23:51 +0900

    refactor : 다중 apk 다중 기기 적용 되도록 수정
    다중으로 문제 사항을 찾도록 변경
    해당 이름은 아직 정하질 못하여 이름은 아직 미정
    추후 이름 변경 필요

--- apk_test.py
+++ apk_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 #-*-coding:utf-8-*-
+import devicefinder
 import apkrunner
+import apkinstaller
 from lib.runprocess import *
 import os
 import screencap
@@ -16,46 +18,70 @@ init()
 
 
 if __name__ == "__main__":
+    device_finder = devicefinder.DeviceFinder()
+    #device_finder.device_list
+    apk_list = list()
+
+
+    apk_installer = apkinstaller.ApkInstaller()
+    apk_list.append(["/Users/numa/temp/KT5/KTAuth_01.00.00_google_20180514_rev39576_DX.apk", "com.kt.mysign.activity.AppSplashActivity"])
+    apk_list.append(["/Users/numa/temp/KT5/ktcs_v05.03.02_onestore_ollehunsigned_20180510_DX.apk", "com.ktshow.cs.Main"])
+    apk_list.append(["/Users/numa/temp/KT5/ktfamilyBox_V03.00.00_onestore_3_2_DX.apk", "com.kt.ollehfamilybox.IntroActivity"])
+    apk_list.append(["/Users/numa/temp/KT5/Ktmembership_AndroidMarket_DX.apk", "com.olleh.android.oc2.MainActivity"])
+    apk_list.append(["/Users/numa/temp/KT5/sign_app-onestore-release-unsigned_DX.apk", "com.kt.android.showtouch.Loading"])
+    for apk, activity in apk_list :
+        #pass
+        apk_installer.apk_install(apk)
+
     
-    #for a in range(100) :
-    while(True) :
-        RunProcessWait("adb logcat -c")
-        apk_runner = apkrunner.ApkRunner()
-        apk_runner.set_apk(["/Users/numa/temp/ez_no_log.apk", ])
-        apk_runner.focused_limit.append("com.lgcns.ucapim.activity.UcapMainTabActivity")
+        for a in range(2) :
+        #while(True) :
+            apk_runner = apkrunner.ApkRunner()
+            apk_runner.set_apk([apk, ])
+            apk_runner.apk_start_activity = activity
 
-        apk_runner.time_limit = 60
-        apk_runner.time_wait = 3
-        apk_runner.apk_running()
-        print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "APK RUNNER END GO FILE CHECK" + Fore.RESET + Back.RESET + Style.NORMAL)
-    
-        
-        focused_list = apk_runner.device_logs.find_event("2165c1582b017ece", "focused_Error")
-        if len(focused_list) > 0 :
-            file_head = "logs"
-            file_tail = ".txt"
-            png_tail = ".png"
-            count = 0
-            while(True)  :
-                print ("FILE UNIQUE SETTING : " + str(count))
-                if os.path.isfile(apk_runner.temp_path + file_head + str(count) + file_tail) == True :
-                    count = count + 1
-                    continue
-                else :
-                    break
-            
-            apk_runner.device_logs.prints("2165c1582b017ece")
+            #apk_runner.focused_limit.append(activity)
 
-            print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "APK Error" + Fore.RESET + Back.RESET + Style.NORMAL)
-            print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "LOG FILE ::: " + apk_runner.temp_path + file_head + str(count) + file_tail + Fore.RESET + Back.RESET + Style.NORMAL)
+            device_finder.find_device_list()
 
-            os.system("adb logcat -d > " + apk_runner.temp_path + file_head + str(count) + file_tail)
-            RunProcessWait("adb logcat -d > " + apk_runner.temp_path + file_head + str(count) + file_tail)
-            screen_cap = screencap.ScreenCap()
-            screen_cap.non_stop_screen_shot("2165c1582b017ece", apk_runner.temp_path, file_head + str(count) + png_tail)
-        print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "ONE STEP" + Fore.RESET + Back.RESET + Style.NORMAL)
-        RunProcessWait("adb shell am force-stop " + apk_runner.apk_name)
-        if os.path.isdir(apk_runner.temp_path + "2165c1582b017ece") :
-            shutil.rmtree(apk_runner.temp_path + "2165c1582b017ece")
-        
-        
+            for device in device_finder.find_list :
+                RunProcessWait("adb -s " + device + " logcat -c")
+            apk_runner.time_limit = 10
+            apk_runner.time_wait = 3
+            apk_runner.apk_running()
+            print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "APK RUNNER END GO FILE CHECK" + Fore.RESET + Back.RESET + Style.NORMAL)
+
+
+            for device in apk_runner.device_finder.find_list :
+                focused_error_list = apk_runner.device_logs.find_event(device, "focused_Error")
+                if len(focused_error_list) > 0 :
+                    file_head = device + "logs"
+                    file_tail = ".txt"
+                    png_tail = ".png"
+                    count = 0
+                    while(True)  :
+                        print ("FILE UNIQUE SETTING : " + str(count))
+                        if os.path.isfile(apk_runner.temp_path + file_head + str(count) + file_tail) == True :
+                            count = count + 1
+                            continue
+                        else :
+                            break
+
+                    print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "APK Error" + Fore.RESET + Back.RESET + Style.NORMAL)
+                    print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "LOG FILE ::: " + apk_runner.temp_path + file_head + str(count) + file_tail + Fore.RESET + Back.RESET + Style.NORMAL)
+
+                    os.system("adb -s " + device + " logcat -d > " + apk_runner.temp_path + file_head + str(count) + file_tail)
+                    RunProcessWait("adb -s " + device + " logcat -d > " + apk_runner.temp_path + file_head + str(count) + file_tail)
+                    screen_cap = screencap.ScreenCap()
+                    screen_cap.non_stop_screen_shot(device, apk_runner.temp_path, file_head + str(count) + png_tail)
+
+
+            for device in apk_runner.device_finder.find_list :
+                print(Fore.CYAN + Back.MAGENTA + Style.BRIGHT + "ONE STEP" + Fore.RESET + Back.RESET + Style.NORMAL)
+                RunProcessWait("adb -s " + device + " shell am force-stop " + apk_runner.apk_name)
+                #ce0416041472263c01
+                #if os.path.isdir(apk_runner.temp_path + "2165c1582b017ece") :
+                    #shutil.rmtree(apk_runner.temp_path + "2165c1582b017ece")
+        apk_installer.apk_uninstall(apk)
+
+
